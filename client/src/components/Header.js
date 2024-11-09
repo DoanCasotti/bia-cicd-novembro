e2c77ec36f8c6c72f4f631363e1c9aa775808486
DoanCasotti <doancasotti@outlook.com>
2024-11-09 16:00:13 -0300

    fazendo o segundo commit alterando cor e texto do botão

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "Black" : "Green"}
-          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "Gray" : "Purple"}
+          text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
       )}
